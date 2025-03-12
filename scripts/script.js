5a5898ff4cbe51514cae2789f32da39873390f5b
tortiman <josejavierbedmar@gmail.com>
2025-03-12 14:03:42 +0100

    erros logic and implementation of errors in html and css

--- scripts/script.js
+++ scripts/script.js
@@ -4,7 +4,9 @@ const fileInput=document.getElementById('fileUpload');
 const textUpload=document.querySelector('.text-upload');
 const buttons=document.querySelector('.botonesRemoveChangeIcon');
 const buttonTicket=document.querySelector('.button-ticket');
-
+const fullName=document.getElementById('name');
+const email=document.getElementById('email');
+const github=document.getElementById('github');
 
 previewContainer.addEventListener('click',(event)=>{
     fileInput.click();
@@ -59,6 +61,7 @@ dropArea.addEventListener('drop',(event)=>{
         previewContainer.src=imgeURL; 
         document.querySelector('.text-info').innerHTML='Upload your photo (JPG or PNG, max size: 500KB).';
         document.querySelector('.text-info').classList.remove('text-info-errors');
+        document.querySelector('.image-info').classList.remove('image-info-error'); 
         textUpload.style.display='none';
         buttons.style.display='flex';
     
@@ -78,10 +81,20 @@ dropArea.addEventListener('drop',(event)=>{
     }
 });
 
+// eventos de que los input box se han rellenado de forma correcta y se quitan los mensajes de error
+
+fullName.addEventListener('focusout',()=>{
+    if(checkTextBox(fullName)){
+        document.querySelector('.error-full-name').classList.add('error-full-name-correct');
+    }else{
+        document.querySelector('.error-full-name').classList.remove('error-full-name-correct');
+    }
+});
+
 buttonTicket.addEventListener('click',()=>{
     // ********logica de errores
     // Comprobacion imagen distinta al icono upload
-    // se cambia el texto de text-info y se pone en rojo
+    // se cambia el texto de text-info y se pone en rojo sino se ha cargado una imagen
     if (previewContainer.src !== 'http://127.0.0.1:3000/assets/images/icon-upload.svg'){
         
     }else{
@@ -89,7 +102,15 @@ buttonTicket.addEventListener('click',()=>{
         document.querySelector('.text-info').classList.add('text-info-errors');
     }
     
-      
+    // Comprobamos que las cajas de texto estan completas
+    
+    if(checkTextBox(fullName)){
+        document.querySelector('.error-full-name').display='none';
+    };
+        
+       
+    checkTextBox(email);
+    checkTextBox(github);  
 });
 
 
@@ -97,6 +118,16 @@ buttonTicket.addEventListener('click',()=>{
 
 function checkSizeFile(file){
     if((file.size/1024)>500){
+        return false;
+    }else{
+        return true;
+    }
+}
+
+// funciones para comprobar que los check estan rellenos y de forma correcta
+
+function checkTextBox(textBox){
+    if(textBox.value.trim()===""){
         return false;
     }else{
         return true;
